0ab0df1e9fb47c39a1b9faf545edd1611931b72a
unknown <adussauge@if501-213-08.insa-lyon.fr>
2016-10-04 09:48:56 +0200

    Personne SOM Added

--- WASO/WASO-SOM-Personne/src/main/java/fr/insalyon/waso/som/personne/ServiceObjetMetier.java
+++ WASO/WASO-SOM-Personne/src/main/java/fr/insalyon/waso/som/personne/ServiceObjetMetier.java
@@ -40,9 +40,54 @@ public class ServiceObjetMetier {
 
             this.container.add("personnes", jsonListe);
 
-        } catch (DBException ex) {
+        }
+        catch (DBException ex) {
             throw new ServiceException("Exception in SOM getListePersonne", ex);
         }
     }
 
+    public void rechercherPersonneParId(int id) throws ServiceException {
+        try {
+            List<Object[]> listePersonne = this.dBConnection.launchQuery("SELECT PersonneID, Nom, Prenom, Mail FROM PERSONNE ORDER BY PersonneID WHERE PersonneID='"+id+"'");
+
+            if(!listePersonne.isEmpty()){
+                Object[] personne = listePersonne.get(0);
+                JsonObject jsonItem = new JsonObject();
+
+                jsonItem.addProperty("id", (Integer) personne[0]);
+                jsonItem.addProperty("nom", (String) personne[1]);
+                jsonItem.addProperty("prenom", (String) personne[2]);
+                jsonItem.addProperty("mail", (String) personne[3]);
+                
+                this.container.add("personnes", jsonItem);
+            }     
+        }
+        catch (DBException ex) {
+            throw new ServiceException("Exception in SOM rechercherPersonneParId", ex);
+        }
+    }
+
+    public void rechercherPersonneParNom(String nom) throws ServiceException {
+        try {
+            List<Object[]> listePersonne = this.dBConnection.launchQuery("SELECT PersonneID, Nom, Prenom, Mail FROM PERSONNE ORDER BY PersonneID WHERE PersonneID='"+nom+"'");
+
+            JsonArray jsonListe = new JsonArray();
+
+            for (Object[] row : listePersonne) {
+                JsonObject jsonItem = new JsonObject();
+
+                jsonItem.addProperty("id", (Integer) row[0]);
+                jsonItem.addProperty("nom", (String) row[1]);
+                jsonItem.addProperty("prenom", (String) row[2]);
+                jsonItem.addProperty("mail", (String) row[3]);
+
+                jsonListe.add(jsonItem);
+            }
+
+            this.container.add("personnes", jsonListe);
+        }
+        catch (DBException ex) {
+            throw new ServiceException("Exception in SOM rechercherPersonneParNom", ex);
+        }
+    }
 }
