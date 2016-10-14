b09d8c69d9f721363738e614f703fc560bbe9a60
unknown <amoll@IF501-210-14.insa-lyon.fr>
2016-10-14 09:38:21 +0200

    Add main prolog

--- ALIA/TP Projet/puissance4.pl
+++ ALIA/TP Projet/puissance4.pl
@@ -2,3 +2,18 @@
 %  Puissance 4 - H4402
 % ----------------------
 
+:- dynamic board/1.
+
+%switch between players
+changePlayer(1,2).
+changePlayer(2,1).
+
+%test if the player Player has won
+win(Board,Player).
+
+play(Player):-  write('New turn for:'), writeln(Player),
+    		board(Board), % instanciate the board from the knowledge base 
+			win(Board, Player). % test if the player Player has won
+
+%start the game! 
+init :- length(Board,42), assert(board(Board)), play(1).
