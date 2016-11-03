5249d65b44302228dfc5b945b3fc52c6e9284b4c
Arnaud <arnaudfavier@users.noreply.github.com>
2016-11-03 09:06:56 +0100

    Put IA MinMax Adrien in a file

---
+++ ALIA/TP Projet/ia_minmax_adrien.pl
@@ -0,0 +1,31 @@
+%%%% AI MinMax Adrien %%%%
+
+playIANegaMax(Board, BestMove, Player):- negaMax(2, Board, Player, Player, _, BestMove).
+
+playMoveIA(Board, Column, NewBoard, Player):-	nth1(Column,Board, E),
+												playColumn(E, L, Player),
+												replaceAtIndex(Board, NewBoard, Column, L).
+% Voir pour changer l'heuristique
+value(Board, MaxPlayer, -999):- changePlayer(MaxPlayer, NextPlayer), winner(Board, NextPlayer).
+value(Board, MaxPlayer, 999):- winner(Board, MaxPlayer).
+value(_, _, 0).
+
+negaMax(0, Board, _, PlayerMax, Value, _):- value(Board, PlayerMax, Value).
+negaMax(_, Board, _, PlayerMax, Value, _):- winner(Board, 1), value(Board, PlayerMax, Value).
+negaMax(_, Board, _, PlayerMax, Value, _):- winner(Board, 2), value(Board, PlayerMax, Value).
+negaMax(Depth, Board, Player, PlayerMax, BestValue, BestMove):-
+	setof(PossibleMove, columnAvailable(Board, PossibleMove), AllPossibleMoves),
+	Depth1 is Depth - 1,
+	negaMax(AllPossibleMoves, Board, Depth1, Player, PlayerMax, BestValue, BestMove, -1000, nil).
+
+negaMax([], _, _, _, _, BestValue, BestMove, BestValue, BestMove).
+negaMax([HMoves|TMoves], Board, Depth, Player, PlayerMax, BestValue, BestMove, BestValueTmp, BestMoveTmp):-
+	playMoveIA(Board, HMoves, NewBoard, Player),
+	changePlayer(Player, NextPlayer),
+	negaMax(Depth, NewBoard, NextPlayer, PlayerMax, NextPlayerBestValue, _), 
+	Value is -NextPlayerBestValue,
+	(	Value > BestValueTmp ->
+		negaMax(TMoves, Board, Depth, Player, PlayerMax, BestValue, BestMove, Value, HMoves)
+	;
+		negaMax(TMoves, Board, Depth, Player, PlayerMax, BestValue, BestMove, BestValueTmp, BestMoveTmp)
+	).
--- ALIA/TP Projet/puissance4.pl
+++ ALIA/TP Projet/puissance4.pl
@@ -5,7 +5,9 @@
 /* ------------------------------ */
 
 % AI files
+% Uncomment to choose
 :- [ia_random].
+%:- [ia_minmax_adrien].
 
 :- dynamic board/1. 
 
@@ -221,41 +223,6 @@ winningMove(ActualBoard, Column, Player):- columnAvailable(ActualBoard, 7), play
 %% Defensive style IA (+++)
 %% Ultimate style IA (merge of =/= styles)
 %%% END TEST VHUGUET %%%%%%%%%%%%%%%
-
-
-%%% MinMax Adrien %%%%%%%%%%%%%%%%%%%
-
-playIANegaMax(Board, BestMove, Player):- negaMax(2, Board, Player, Player, _, BestMove).
-
-playMoveIA(Board, Column, NewBoard, Player):- 	nth1(Column,Board, E),
-						playColumn(E, L, Player),
-						replaceAtIndex(Board, NewBoard, Column, L).
-%Voir pour changer l'heuristique
-value(Board, MaxPlayer, -999):- changePlayer(MaxPlayer, NextPlayer), winner(Board, NextPlayer).
-value(Board, MaxPlayer, 999):- winner(Board, MaxPlayer).
-value(_, _, 0).
-
-negaMax(0, Board, _, PlayerMax, Value, _):- value(Board, PlayerMax, Value).
-negaMax(_, Board, _, PlayerMax, Value, _):- winner(Board, 1), value(Board, PlayerMax, Value).
-negaMax(_, Board, _, PlayerMax, Value, _):- winner(Board, 2), value(Board, PlayerMax, Value).
-negaMax(Depth, Board, Player, PlayerMax, BestValue, BestMove):-
-	setof(PossibleMove, columnAvailable(Board, PossibleMove), AllPossibleMoves),
-	Depth1 is Depth - 1,
-	negaMax(AllPossibleMoves, Board, Depth1, Player, PlayerMax, BestValue, BestMove, -1000, nil).
-
-negaMax([], _, _, _, _, BestValue, BestMove, BestValue, BestMove).
-negaMax([HMoves|TMoves], Board, Depth, Player, PlayerMax, BestValue, BestMove, BestValueTmp, BestMoveTmp):-
-	playMoveIA(Board, HMoves, NewBoard, Player),
-	changePlayer(Player, NextPlayer),
-	negaMax(Depth, NewBoard, NextPlayer, PlayerMax, NextPlayerBestValue, _), 
-	Value is -NextPlayerBestValue,
-	(	Value > BestValueTmp ->
-		negaMax(TMoves, Board, Depth, Player, PlayerMax, BestValue, BestMove, Value, HMoves)
-	;
-		negaMax(TMoves, Board, Depth, Player, PlayerMax, BestValue, BestMove, BestValueTmp, BestMoveTmp)
-	).
-
-%%% END MinMax %%%%%%%%%%%%%%%
 
 %%% TEST MINIMAX ADUSSAUGE  %%%
 
