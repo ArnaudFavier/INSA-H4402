00135c89c66d88d4d4fd280811fb727da4b7c4c9
admo120104 <adrien.moll@insa-lyon.fr>
2016-11-03 04:28:26 +0100

    Add minmax

--- ALIA/TP Projet/puissance4.pl
+++ ALIA/TP Projet/puissance4.pl
@@ -195,7 +195,7 @@ nonFilledColumnIds(Board, Res):- findall(I, (length(Board, Long), between(1, Lon
 columnAvailable(Board, Column):- nth1(Column, Board, List), member(0, List).
 
 %% Completely Random 
-playIA(Board, Move, _):- repeat, Move is random(7), nth1(Move, Board, X), nth1(_, X, 0), !.
+playIA(Board, Move, _):- repeat, M is random(7), Move is M + 1, columnAvailable(Board, Move), !.
 
 %% Winning move
 % AI that identifies a winning move for the player (1 or 2)
@@ -228,6 +228,41 @@ winningMove(ActualBoard, Column, Player):- columnAvailable(ActualBoard, 7), play
 %% Defensive style IA (+++)
 %% Ultimate style IA (merge of =/= styles)
 %%% END TEST VHUGUET %%%%%%%%%%%%%%%
+
+
+%%% MinMax Adrien %%%%%%%%%%%%%%%%%%%
+
+playIANegaMax(Board, BestMove, Player):- negaMax(2, Board, Player, Player, _, BestMove).
+
+playMoveIA(Board, Column, NewBoard, Player):- 	nth1(Column,Board, E),
+						playColumn(E, L, Player),
+						replaceAtIndex(Board, NewBoard, Column, L).
+%Voir pour changer l'heuristique
+value(Board, MaxPlayer, -999):- changePlayer(MaxPlayer, NextPlayer), winner(Board, NextPlayer).
+value(Board, MaxPlayer, 999):- winner(Board, MaxPlayer).
+value(_, _, 0).
+
+negaMax(0, Board, _, PlayerMax, Value, _):- value(Board, PlayerMax, Value).
+negaMax(_, Board, _, PlayerMax, Value, _):- winner(Board, 1), value(Board, PlayerMax, Value).
+negaMax(_, Board, _, PlayerMax, Value, _):- winner(Board, 2), value(Board, PlayerMax, Value).
+negaMax(Depth, Board, Player, PlayerMax, BestValue, BestMove):-
+	setof(PossibleMove, columnAvailable(Board, PossibleMove), AllPossibleMoves),
+	Depth1 is Depth - 1,
+	negaMax(AllPossibleMoves, Board, Depth1, Player, PlayerMax, BestValue, BestMove, -1000, nil).
+
+negaMax([], _, _, _, _, BestValue, BestMove, BestValue, BestMove).
+negaMax([HMoves|TMoves], Board, Depth, Player, PlayerMax, BestValue, BestMove, BestValueTmp, BestMoveTmp):-
+	playMoveIA(Board, HMoves, NewBoard, Player),
+	changePlayer(Player, NextPlayer),
+	negaMax(Depth, NewBoard, NextPlayer, PlayerMax, NextPlayerBestValue, _), 
+	Value is -NextPlayerBestValue,
+	(	Value > BestValueTmp ->
+		negaMax(TMoves, Board, Depth, Player, PlayerMax, BestValue, BestMove, Value, HMoves)
+	;
+		negaMax(TMoves, Board, Depth, Player, PlayerMax, BestValue, BestMove, BestValueTmp, BestMoveTmp)
+	).
+
+%%% END MinMax %%%%%%%%%%%%%%%
 
 %%% TEST MINIMAX ADUSSAUGE  %%%
 
