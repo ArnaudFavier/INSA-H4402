d470d0d2c29c856b85d8439e6218b97b93377108
Setny <anthony.delay@outlook.fr>
2016-11-09 08:27:20 +0100

    Update last commit

--- WebSemantique/src/main/java/semanticweb/controllers/SearchController.java
+++ WebSemantique/src/main/java/semanticweb/controllers/SearchController.java
@@ -5,14 +5,21 @@ import com.ibm.watson.developer_cloud.alchemy.v1.model.Keyword;
 import org.apache.commons.validator.routines.UrlValidator;
 import semanticweb.services.Services;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.*;
 
 /**
  * This controller can be used in our web page to get expected results when research in triggered
  */
 public class SearchController {
-
+	
 	/**
 	 * For the moment, this method only return a list of urls from Google results
 	 *
@@ -46,4 +53,16 @@ public class SearchController {
 
 		return alchemyTexts;
 	}
+	
+	public List<String> getURIsFromTexts(List<String> texts, String confidence) throws Exception {
+        List<String> uris = Services.getURIsFromTexts(texts, confidence);
+        
+        // Debug
+        System.out.println("URIS");
+        for(String uri : uris) {
+        	System.out.println(uri);
+        }
+        
+        return uris;
+    }
 }
--- WebSemantique/src/main/java/semanticweb/services/Services.java
+++ WebSemantique/src/main/java/semanticweb/services/Services.java
@@ -10,15 +10,25 @@ import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import semanticweb.References;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.*;
 
 /**
  * Contains some services usable by controllers
  */
 public class Services {
+	
+	private static final String dbpediaSpotlightUrl = "http://spotlight.sztaki.hu:2222/rest/annotate";
 
 	/**
 	 * @param searchString The string given to Google
@@ -61,6 +71,74 @@ public class Services {
 		}
 
 		return texts;
+	}
+	
+	/**
+	 * 
+	 * @param A text associated with an url
+	 * @param confidence The confidence range
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<String> getURIsFromTexts(List<String> texts, String confidence) throws Exception {
+		
+		// FOR DEBUG : TODO find why its doesn't work with real texts
+		texts = new ArrayList<String>();
+		texts.add("President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance");
+		
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(dbpediaSpotlightUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Accept", "application/json"); //header format accepted
+        conn.connect();
+        
+        List<String> uris = new ArrayList<String>();
+
+        for(String text : texts) {
+	        //formulating request
+	        String request = "text= " + text;
+	        request += "&confidence= " + confidence;
+	        
+	        byte[] inputBytes = request.getBytes("UTF-8");
+	        OutputStream os = conn.getOutputStream();
+	        os.write(inputBytes);
+	        os.close();
+	
+	
+	        //result of request in json
+	        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        String line;
+	        while ((line = rd.readLine()) != null) {
+	            result.append(line);
+	        }
+	        rd.close();
+	        
+	        
+	        String jsonString = result.toString();
+	        JSONObject jsonObject = new JSONObject(jsonString);
+	        JSONArray resources;
+	        try {
+	        	 resources = jsonObject.getJSONArray("Resources");
+	        }
+	        catch (Exception e) {
+	        	System.err.println(e.getMessage());
+	        	e.printStackTrace();
+	        	resources = null;
+	        }
+	        
+	        int i = 0;
+	        JSONObject resource = null;
+	        
+	        while(i < resources.length()) {
+	        	resource = (JSONObject) resources.get(i);
+	        	uris.add(resource.getString("@URI"));
+	        	i++;
+	        }
+        }
+        
+        return uris;
 	}
 
 	public static void sparqlRDFTripletFromUri (List<String> uri){
