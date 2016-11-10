4f8b4975e471f1a76344a09887eca8d478911a46
Aurélien Dussauge <aurelien.dussauge@gmail.com>
2016-11-10 10:10:26 +0100

    Add class RDFTriplet

---
+++ WebSemantique/src/main/java/semanticweb/model/RDFTriplet.java
@@ -0,0 +1,38 @@
+package semanticweb.model;
+
+import org.apache.jena.rdf.model.RDFNode;
+
+/**
+ * Created by adussauge on 10/11/16.
+ */
+public class RDFTriplet {
+    private String uri;
+    private String predicate;
+    private String object;
+
+    public RDFTriplet(String uri, RDFNode predicate, RDFNode object) {
+        this.uri = uri;
+        this.predicate = predicate.toString();
+        this.object = object.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RDFTriplet that = (RDFTriplet) o;
+
+        if (predicate != null ? !predicate.equals(that.predicate) : that.predicate != null) return false;
+        return object != null ? object.equals(that.object) : that.object == null;
+    }
+
+    @Override
+    public String toString() {
+        return "RDFTriplet{" +
+                "uri='" + uri + '\'' +
+                ", predicate='" + predicate + '\'' +
+                ", object='" + object + '\'' +
+                '}';
+    }
+}
--- WebSemantique/src/main/java/semanticweb/services/Services.java
+++ WebSemantique/src/main/java/semanticweb/services/Services.java
@@ -7,13 +7,12 @@ import com.google.api.services.customsearch.model.Result;
 import com.google.api.services.customsearch.model.Search;
 import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.Resource;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import semanticweb.References;
+import semanticweb.model.RDFTriplet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,63 +26,64 @@ import java.util.*;
  * Contains some services usable by controllers
  */
 public class Services {
-	
-	private static final String dbpediaSpotlightUrl = "http://spotlight.sztaki.hu:2222/rest/annotate";
 
-	/**
-	 * @param searchString The string given to Google
-	 * @return A list of Google results, resulting of the Google research
-	 */
-	public static List<Result> getGoogleResultsFromString(String searchString) {
-		Customsearch customsearch = new Customsearch(new NetHttpTransport(), new JacksonFactory(), null);
+    private static final String dbpediaSpotlightUrl = "http://spotlight.sztaki.hu:2222/rest/annotate";
 
-		List<Result> results = null;
-		try {
-			com.google.api.services.customsearch.Customsearch.Cse.List list = customsearch.cse().list(searchString);
-			list.setKey(References.GOOGLE_API_KEY);
-			list.setCx(References.GOOGLE_SEARCH_ENGINE_ID);
-			Search search = list.execute();
-			results = search.getItems();
-		} catch (IOException ex) {
-			System.err.println(ex);
-		}
-		return results;
-	}
+    /**
+     * @param searchString The string given to Google
+     * @return A list of Google results, resulting of the Google research
+     */
+    public static List<Result> getGoogleResultsFromString(String searchString) {
+        Customsearch customsearch = new Customsearch(new NetHttpTransport(), new JacksonFactory(), null);
 
-	/**
-	 * @param urls List of URL
-	 * @return A list of texts, created by Alchemy from a list of URL
-	 */
-	public static List<String> getTextsFromUrls(List<String> urls) {
-		final AlchemyLanguage language = new AlchemyLanguage();
-		language.setApiKey(References.ALCHEMY_API_KEY);
+        List<Result> results = null;
+        try {
+            com.google.api.services.customsearch.Customsearch.Cse.List list = customsearch.cse().list(searchString);
+            list.setKey(References.GOOGLE_API_KEY);
+            list.setCx(References.GOOGLE_SEARCH_ENGINE_ID);
+            Search search = list.execute();
+            results = search.getItems();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+        return results;
+    }
 
-		List<String> texts = new ArrayList<>(urls.size());
+    /**
+     * @param urls List of URL
+     * @return A list of texts, created by Alchemy from a list of URL
+     */
+    public static List<String> getTextsFromUrls(List<String> urls) {
+        final AlchemyLanguage language = new AlchemyLanguage();
+        language.setApiKey(References.ALCHEMY_API_KEY);
 
-		for (String url : urls) {
-			try {
-				texts.add(language.getText(new HashMap<String, Object>(){{
-					put(AlchemyLanguage.URL, url);
-				}}).execute().getText());
-			} catch(Exception e) {
-				texts.add("");
-			}
-		}
+        List<String> texts = new ArrayList<>(urls.size());
 
-		return texts;
-	}
-	
-	/**
-	 * 
-	 * @param A text associated with an url
-	 * @param confidence The confidence range
-	 * @return
-	 * @throws Exception
-	 */
-	public static List<String> getURIsFromTexts(List<String> texts, String confidence) throws Exception {
+        for (String url : urls) {
+            try {
+                texts.add(language.getText(new HashMap<String, Object>() {{
+                    put(AlchemyLanguage.URL, url);
+                }}).execute().getText());
+            } catch (Exception e) {
+                texts.add("");
+            }
+        }
 
-		texts.add("President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance");
-		
+        return texts;
+    }
+
+    /**
+     * @param texts      A text associated with an url
+     * @param confidence The confidence range
+     * @return
+     * @throws Exception
+     */
+    public static List<String> getURIsFromTexts(List<String> texts, String confidence) throws Exception {
+
+        // FOR DEBUG : TODO find why its doesn't work with real texts
+        texts = new ArrayList<String>();
+        texts.add("President Obama called Wednesday on Cresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresresident Obama called Wednesday on Congresongress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance");
+
         StringBuilder result = new StringBuilder();
         URL url = new URL(dbpediaSpotlightUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -91,94 +91,100 @@ public class Services {
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Accept", "application/json"); //header format accepted
         conn.connect();
-        
+
         List<String> uris = new ArrayList<String>();
 
-        for(String text : texts) {
-	        //formulating request
-	        String request = "text= " + text;
-	        request += "&confidence= " + confidence;
-	        
-	        byte[] inputBytes = request.getBytes("UTF-8");
-	        OutputStream os = conn.getOutputStream();
-	        os.write(inputBytes);
-	        os.close();
-	
-	
-	        //result of request in json
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-	        String line;
-	        while ((line = rd.readLine()) != null) {
-	            result.append(line);
-	        }
-	        rd.close();
-	        
-	        
-	        String jsonString = result.toString();
-	        JSONObject jsonObject = new JSONObject(jsonString);
-	        JSONArray resources;
-	        try {
-	        	 resources = jsonObject.getJSONArray("Resources");
-	        }
-	        catch (Exception e) {
-	        	System.err.println(e.getMessage());
-	        	e.printStackTrace();
-	        	resources = null;
-	        }
-	        
-	        int i = 0;
-	        JSONObject resource = null;
-	        
-	        while(i < resources.length()) {
-	        	resource = (JSONObject) resources.get(i);
-	        	uris.add(resource.getString("@URI"));
-	        	i++;
-	        }
+        for (String text : texts) {
+            //formulating request
+            String request = "text= " + text;
+            request += "&confidence= " + confidence;
+
+            byte[] inputBytes = request.getBytes("UTF-8");
+            OutputStream os = conn.getOutputStream();
+            os.write(inputBytes);
+            os.close();
+
+
+            //result of request in json
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+
+
+            String jsonString = result.toString();
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray resources;
+            try {
+                resources = jsonObject.getJSONArray("Resources");
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+                e.printStackTrace();
+                resources = null;
+            }
+
+            int i = 0;
+            JSONObject resource = null;
+
+            while (i < resources.length()) {
+                resource = (JSONObject) resources.get(i);
+                uris.add(resource.getString("@URI"));
+                i++;
+            }
         }
-        
+
         return uris;
-	}
+    }
 
-	public static Map<String, List<RDFNode>> sparqlRDFTripletFromUri (List<String> uris){
-		System.out.println("SPARQL:");
+    public static List<RDFTriplet> sparqlRDFTripletFromUri(List<String> uris) {
+        System.out.println("SPARQL:");
 
-		Map<String, List<RDFNode>> listeTriplets = new HashMap<>();
+        List<RDFTriplet> listeTriplets = new ArrayList<>();
 
-		for (String uri : uris) {
-			String queryString =
-				"PREFIX : <http://dbpedia.org/resource/>\n" +
-				"SELECT * WHERE {\n" +
-				":" + uri + " ?p ?o\n" +
-				"}";
+        for (String uri : uris) {
+            String queryString =
+                    "PREFIX : <http://dbpedia.org/resource/>\n" +
+                            "SELECT * WHERE {\n" +
+                            ":" + uri + " ?p ?o\n" +
+                            "}";
 
-			Query query = QueryFactory.create(queryString);
+            Query query = QueryFactory.create(queryString);
 
-			QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+            QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
 
-			List<RDFNode> listeRDFNode = new ArrayList<>();
+            try {
+                ResultSet results = qexec.execSelect();
 
-			try {
-				ResultSet results = qexec.execSelect();
+                for (; results.hasNext(); ) {
+                    QuerySolution qsol = results.nextSolution();
 
-				for (; results.hasNext(); ) {
-					QuerySolution qsol = results.nextSolution();
+                    try {
+                        RDFNode obj = qsol.get("o");
+                        RDFNode predicate = qsol.get("p");
+                        RDFTriplet triplet = new RDFTriplet(uri, predicate, obj);
+                        System.out.println(triplet);
+                        listeTriplets.add(triplet);
+                    } catch (Exception e) {
+                        System.err.println(e);
+                    }
+                }
+            } finally {
+                qexec.close();
+            }
+        }
 
-					try {
-						RDFNode r = qsol.get("o");
-						System.out.println("RDF Node: \t" + r);
-						listeRDFNode.add(r);
-					} catch (Exception e) {}
-				}
-			} finally {
-				qexec.close();
-			}
+        return listeTriplets;
+    }
 
-			listeTriplets.put(uri,listeRDFNode);
-		}
-
-		return listeTriplets;
-	}
-
-
+    /**
+     * @param tripletsForUrl a map linking each url to its uris
+     * @return a similarity matrix between each URL. 1 = All RDP triplet are the same
+     * @see <a href="https://fr.wikipedia.org/wiki/Indice_et_distance_de_Jaccard">Jaccard Distance</a>
+     */
+    public static float[][] computeJaccardMatrix(Map<String, List<RDFNode>> tripletsForUrl) {
+        return null;
+    }
 
 }
