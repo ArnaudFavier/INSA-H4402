2cab5d54a1a50d66d50c96a35a1d0b151818ef19
Aurélien Dussauge <aurelien.dussauge@gmail.com>
2016-11-17 16:37:43 +0100

    Add Main

---
+++ Agile/src/main/java/agile/Main.java
@@ -0,0 +1,12 @@
+package agile;
+
+import org.junit.Test;
+
+import com.google.java.contract.Ensures;
+import com.google.java.contract.Requires;
+
+public class Main {
+	public static final void main(String[] args) {
+		System.out.println("Success");
+	}
+}
