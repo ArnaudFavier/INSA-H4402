9dd7915a44422d2453626741397a9173065ab32f
rimokdad <rim.el-idrissi-mokdad@insa-lyon.fr>
2016-11-21 09:59:15 +0100

    add all needed methods

--- Agile/src/main/java/agile/controlleur/Etat.java
+++ Agile/src/main/java/agile/controlleur/Etat.java
@@ -1,5 +1,24 @@
 package agile.controlleur;
 
-public interface Etat {
+import agile.modele.Plan;
+import agile.xml.DeserialiseurPlanXML;
 
+public interface Etat {
+    public void chargerPlan(Controlleur controlleur);
+
+    public void chargerDemandeLivraison(Controlleur controlleur);
+
+    public void calculerTournee(Controlleur controlleur);
+
+    public void genererFeuilleDeRoute(Controlleur controlleur);
+
+    public void enregistrerFeuilleDeRoute(Controlleur controlleur);
+
+    public void ajouterLivraison(Controlleur controlleur);
+
+    public void modifierLivraison(Controlleur controlleur);
+
+    public void undo(Historique historique);
+
+    public void redo(Historique historique);
 }
