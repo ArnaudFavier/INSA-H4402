5c06ebf325084e166c887bd32f29c3dee075f71e
Setny <anthony.delay@outlook.fr>
2016-11-21 10:06:51 +0100

    Implements interface Etat

--- Agile/src/main/java/agile/controlleur/EtatDefaut.java
+++ Agile/src/main/java/agile/controlleur/EtatDefaut.java
@@ -2,10 +2,64 @@ package agile.controlleur;
 
 public abstract class EtatDefaut implements Etat {
 
-    /**
-     * Default constructor
-     */
-    public EtatDefaut() {
-    }
+	/**
+	 * Default constructor
+	 */
+	public EtatDefaut() {
+	}
+
+	@Override
+	public void chargerPlan(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void chargerDemandeLivraison(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void calculerTournee(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void genererFeuilleDeRoute(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void enregistrerFeuilleDeRoute(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ajouterLivraison(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modifierLivraison(Controlleur controlleur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo(Historique historique) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void redo(Historique historique) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
