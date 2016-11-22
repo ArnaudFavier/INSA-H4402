ff52b84350304a9c6211452d09d6959e686b28cc
Setny <anthony.delay@outlook.fr>
2016-11-22 14:17:50 +0100

    Update Tournee

--- Agile/src/test/java/agile/modele/TourneeTest.java
+++ Agile/src/test/java/agile/modele/TourneeTest.java
@@ -1,6 +1,6 @@
 package agile.modele;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,51 +17,51 @@ import org.xml.sax.SAXException;
 import agile.xml.DeserialiseurDemandeLivraisonsXML;
 import agile.xml.DeserialiseurPlanXML;
 
-public class DemandeLivraisonsTest {
+public class TourneeTest {
 
-    @Test(timeout = 10000)
-    public void testChargerDemandeDeLivraison()
-	    throws URISyntaxException, SAXException, ParserConfigurationException, IOException {
-	URL urlPlan = getClass().getResource("plan5x5.xml");
-	File xmlPlan = new File(urlPlan.toURI());
-	Plan plan = DeserialiseurPlanXML.charger(xmlPlan);
+	@Test(timeout = 10000)
+	public void testChargerDemandeDeLivraison()
+			throws URISyntaxException, SAXException, ParserConfigurationException, IOException {
+		URL urlPlan = getClass().getResource("plan5x5.xml");
+		File xmlPlan = new File(urlPlan.toURI());
+		Plan plan = DeserialiseurPlanXML.charger(xmlPlan);
 
-	URL urlDemandeLivraison = getClass().getResource("livraisons5x5-4.xml");
-	File xmlDemandeLivraison = new File(urlDemandeLivraison.toURI());
-	DemandeLivraisons demandeLivraison = DeserialiseurDemandeLivraisonsXML.charger(xmlDemandeLivraison, plan);
+		URL urlDemandeLivraison = getClass().getResource("livraisons5x5-4.xml");
+		File xmlDemandeLivraison = new File(urlDemandeLivraison.toURI());
+		DemandeLivraisons demandeLivraison = DeserialiseurDemandeLivraisonsXML.charger(xmlDemandeLivraison, plan);
 
-	DemandeLivraisonsTraitee demandeLivraisonsTraitee = new DemandeLivraisonsTraitee(demandeLivraison);
-	demandeLivraisonsTraitee.calculerTSP();
+		Tournee tournee = new Tournee(demandeLivraison);
+		tournee.calculerTSP();
 
-	List<Intersection> intersections = demandeLivraisonsTraitee.getListeIntersectionsTSP();
-	List<Integer> intersectionsId = new ArrayList<>();
-	for (Intersection intersection : intersections) {
-	    intersectionsId.add(intersection.getId());
+		List<Intersection> intersections = tournee.getListeIntersectionsTSP();
+		List<Integer> intersectionsId = new ArrayList<>();
+		for (Intersection intersection : intersections) {
+			intersectionsId.add(intersection.getId());
+		}
+
+		List<Integer> expectedIntersectionsId = new ArrayList<>();
+		expectedIntersectionsId.add(21);
+		expectedIntersectionsId.add(16);
+		expectedIntersectionsId.add(11);
+		expectedIntersectionsId.add(12);
+		expectedIntersectionsId.add(13);
+		expectedIntersectionsId.add(8);
+		expectedIntersectionsId.add(7);
+		expectedIntersectionsId.add(2);
+		expectedIntersectionsId.add(3);
+		expectedIntersectionsId.add(4);
+		expectedIntersectionsId.add(9);
+		expectedIntersectionsId.add(4);
+		expectedIntersectionsId.add(3);
+		expectedIntersectionsId.add(2);
+		expectedIntersectionsId.add(1);
+		expectedIntersectionsId.add(0);
+		expectedIntersectionsId.add(5);
+		expectedIntersectionsId.add(10);
+		expectedIntersectionsId.add(11);
+		expectedIntersectionsId.add(16);
+		expectedIntersectionsId.add(21);
+
+		assertEquals(expectedIntersectionsId, intersectionsId);
 	}
-	
-	List<Integer> expectedIntersectionsId = new ArrayList<>();
-	expectedIntersectionsId.add(21);
-	expectedIntersectionsId.add(16);
-	expectedIntersectionsId.add(11);
-	expectedIntersectionsId.add(12);
-	expectedIntersectionsId.add(13);
-	expectedIntersectionsId.add(8);
-	expectedIntersectionsId.add(7);
-	expectedIntersectionsId.add(2);
-	expectedIntersectionsId.add(3);
-	expectedIntersectionsId.add(4);
-	expectedIntersectionsId.add(9);
-	expectedIntersectionsId.add(4);
-	expectedIntersectionsId.add(3);
-	expectedIntersectionsId.add(2);
-	expectedIntersectionsId.add(1);
-	expectedIntersectionsId.add(0);
-	expectedIntersectionsId.add(5);
-	expectedIntersectionsId.add(10);
-	expectedIntersectionsId.add(11);
-	expectedIntersectionsId.add(16);
-	expectedIntersectionsId.add(21);
-	
-	assertEquals(expectedIntersectionsId, intersectionsId);
-    }
 }
