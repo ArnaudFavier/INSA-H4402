3b617c56c40d76bb196d4dc9ffe861a3745d50c1
HuguetValentin <valentin.huguet@insa-lyon.fr>
2016-11-23 09:33:56 +0100

    Add check cancelling  of file choosing

--- Agile/src/main/java/agile/controlleur/EtatTourneeCalculee.java
+++ Agile/src/main/java/agile/controlleur/EtatTourneeCalculee.java
@@ -17,9 +17,9 @@ import javafx.stage.FileChooser;
 
 public class EtatTourneeCalculee extends EtatDefaut {
 
-	private static String NOM_FICHIER = "tournee";
-	private static String EXTENSION_FICHIER = ".txt";
-	private static String CHEMIN = "src/main/resources/";
+	// private static String NOM_FICHIER = "tournee";
+	// private static String EXTENSION_FICHIER = ".txt";
+	// private static String CHEMIN = "src/main/resources/";
 
 	public EtatTourneeCalculee() {
 	}
@@ -29,89 +29,93 @@ public class EtatTourneeCalculee extends EtatDefaut {
 
 		FileChooser fileChooserTXT = new FileChooser();
 		fileChooserTXT.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Fichier texte", "*.txt"));
-		// File fichier = fileChooserTXT.showOpenDialog(null);
 		File fichier = fileChooserTXT.showSaveDialog(null);
 
-		if (fichier != null) {
+		if (fichier.equals(null)) {
+			System.out.println("Erreur: Aucun fichier pour l'export n'a été séléctionné.");
+		} else {
+			if (fichier != null) {
+				try {
+					fichier.createNewFile();
+				} catch (IOException e) {
+					System.err.println("Impossible de créer le fichier d'export " + fichier.toString());
+					System.err.println(e.getMessage());
+					e.printStackTrace();
+				}
+			}
+
+			Tournee tournee = controlleur.getTournee();
+
+			// SimpleDateFormat formatDate = new
+			// SimpleDateFormat("dd-MM-yyyy_HH-mm");
+			// String date = formatDate.format(new Date());
+			// String nomFichier = NOM_FICHIER + date + EXTENSION_FICHIER;
+			// String fichier = CHEMIN + nomFichier;
+
 			try {
-				fichier.createNewFile();
-			} catch (IOException e) {
-				System.err.println("Impossible de créer le fichier d'export " + fichier.toString());
+				// Création et ouverture du fichier texte
+				PrintWriter out = new PrintWriter(new FileWriter(fichier));
+
+				// En-tête
+				out.println("\t\t\tTOURNEE");
+				SimpleDateFormat formatDate = new SimpleDateFormat("dd MMMM yyyy 'à' hh:mm:ss");
+				String date = formatDate.format(new Date());
+				out.println("Editée le: " + date);
+				out.println();
+
+				// Contenu
+
+				Entrepot entrepot = tournee.getDemandeInitiale().getEntrepot();
+				int etapeActuelle = 1;
+				String derniereRue = null;
+
+				out.println("Départ de l'entrepôt[" + entrepot.getIntersection().getId() + "] à "
+						+ entrepot.getHeureDepart().toString() + ".");
+
+				Iterator<Livraison> iterator = tournee.getLivraisonsTSP().iterator();
+
+				for (Chemin chemin : tournee.getCheminsTSP()) {
+					// Description du chemin
+					for (Troncon troncon : chemin.getTroncons()) {
+						out.print(etapeActuelle + ". ");
+						if (troncon.getNomRue().equals(derniereRue)) {
+							out.print("Continuer sur ");
+						} else {
+							out.print("Prendre ");
+						}
+						out.println(troncon.getNomRue() + ".");
+						derniereRue = troncon.getNomRue();
+						etapeActuelle++;
+					}
+
+					// Description de la livraison
+					if (iterator.hasNext()) {
+						Livraison livraison = iterator.next();
+						out.print("\t====> Effectuer la livraison à " + livraison.getIntersection().getId());
+						if (livraison.ContrainteDeTemps() == true) {
+							out.print(" entre " + livraison.getDebutPlage().toString() + " et "
+									+ livraison.getFinPlage().toString());
+						}
+						out.println(". [Durée de la livraison: " + livraison.getDuree() + " secondes]");
+						derniereRue = null;
+					}
+				}
+
+				out.println("\t====> Arrivée à l'entrepôt[" + entrepot.getIntersection().getId() + "].");
+				out.println();
+				out.println("Fin de la tournée.");
+
+				// Fermeture du fichier
+				out.close();
+				System.out.println("La tournée a été exportée dans " + fichier.toString() + ".");
+			} catch (Exception e) {
+				System.err.println("Erreur lors de l'export de la tournée");
 				System.err.println(e.getMessage());
 				e.printStackTrace();
 			}
+
+			System.out.println("Enregistrement de la feuille de route");
 		}
-
-		Tournee tournee = controlleur.getTournee();
-
-		SimpleDateFormat formatDate = new SimpleDateFormat("dd-MM-yyyy_HH-mm");
-		String date = formatDate.format(new Date());
-		String nomFichier = NOM_FICHIER + date + EXTENSION_FICHIER;
-		// String fichier = CHEMIN + nomFichier;
-
-		try {
-			// Création et ouverture du fichier texte
-			PrintWriter out = new PrintWriter(new FileWriter(fichier));
-
-			// En-tête
-			out.println("\t\t\tTOURNEE");
-			formatDate = new SimpleDateFormat("dd MMMM yyyy 'à' hh:mm:ss");
-			date = formatDate.format(new Date());
-			out.println("Editée le: " + date);
-			out.println();
-
-			// Contenu
-
-			Entrepot entrepot = tournee.getDemandeInitiale().getEntrepot();
-			int etapeActuelle = 1;
-			String derniereRue = null;
-
-			out.println("Départ de l'entrepôt[" + entrepot.getIntersection().getId() + "] à "
-					+ entrepot.getHeureDepart().toString() + ".");
-
-			Iterator<Livraison> iterator = tournee.getLivraisonsTSP().iterator();
-
-			for (Chemin chemin : tournee.getCheminsTSP()) {
-				// Description du chemin
-				for (Troncon troncon : chemin.getTroncons()) {
-					out.print(etapeActuelle + ". ");
-					if (troncon.getNomRue().equals(derniereRue)) {
-						out.print("Continuer sur ");
-					} else {
-						out.print("Prendre ");
-					}
-					out.println(troncon.getNomRue() + ".");
-					derniereRue = troncon.getNomRue();
-					etapeActuelle++;
-				}
-
-				// Description de la livraison
-				if (iterator.hasNext()) {
-					Livraison livraison = iterator.next();
-					out.print("\t====> Effectuer la livraison à " + livraison.getIntersection().getId());
-					if (livraison.ContrainteDeTemps() == true) {
-						out.print(" entre " + livraison.getDebutPlage().toString() + " et "
-								+ livraison.getFinPlage().toString());
-					}
-					out.println(". [Durée de la livraison: " + livraison.getDuree() + " secondes]");
-					derniereRue = null;
-				}
-			}
-
-			out.println("\t====> Arrivée à l'entrepôt[" + entrepot.getIntersection().getId() + "].");
-			out.println();
-			out.println("Fin de la tournée.");
-
-			// Fermeture du fichier
-			out.close();
-			System.out.println("La tournée a été exportée dans " + fichier.toString() + ".");
-		} catch (Exception e) {
-			System.err.println("Erreur lors de l'export de la tournée");
-			System.err.println(e.getMessage());
-			e.printStackTrace();
-		}
-
-		System.out.println("Enregistrement de la feuille de route");
 	}
 
 }
