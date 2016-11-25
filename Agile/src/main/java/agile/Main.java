6b5bbe4278823cae9da05f8dae9046d4b0bedd19
Arnaud <arnaudfavier@users.noreply.github.com>
2016-11-25 13:59:48 +0100

    Add Javadoc Main

--- Agile/src/main/java/agile/Main.java
+++ Agile/src/main/java/agile/Main.java
@@ -5,14 +5,33 @@ import agile.vue.Fenetre;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * La classe Main hérite de Application (JavaFX)
+ *
+ */
 public class Main extends Application {
 
-	Fenetre fenetre;
+	/**
+	 * La fenêtre graphique principale de l'application
+	 */
+	private Fenetre fenetre;
 
+	/**
+	 * Première méthode appelée, lance la méthode start()
+	 * 
+	 * @param args
+	 *            non utilisé
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	/**
+	 * Instancie le controlleur général de l'application et la fenêtre graphique
+	 * 
+	 * @param primaryStage
+	 *            Première stage de l'application (générée par JavaFX)
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Controlleur controlleur = new Controlleur();
