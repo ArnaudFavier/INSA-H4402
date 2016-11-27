96200fe8427aa2003272505fa2527ca90e43cbba
Arnaud <arnaudfavier@users.noreply.github.com>
2016-11-27 21:38:22 +0100

    Add some Javadoc

--- Agile/src/main/java/agile/vue/EntrepotVue.java
+++ Agile/src/main/java/agile/vue/EntrepotVue.java
@@ -6,15 +6,25 @@ import agile.modele.Entrepot;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+/**
+ * Container formattant les données d'un entrepôt pour les afficher dans un
+ * tableau (TreeTableView)
+ */
 public class EntrepotVue extends RecursiveTreeObject<EntrepotVue> {
+    // Informations à afficher
+    public StringProperty intersection;
+    public StringProperty heureDepart;
 
-	StringProperty intersection;
-	StringProperty heureDepart;
-
-	public EntrepotVue(Entrepot entrepot) {
-		this.intersection = new SimpleStringProperty(entrepot.getIntersection().getId() + " ("
-				+ entrepot.getIntersection().getX() + ", " + entrepot.getIntersection().getY() + ")");
-		this.heureDepart = new SimpleStringProperty(entrepot.getHeureDepart().toString());
-	}
-
+    /**
+     * Constructeur liant les informations à afficher de l'entrepôt passé en
+     * paramètre
+     * 
+     * @param entrepot
+     *            Entrepôt à afficher
+     */
+    public EntrepotVue(Entrepot entrepot) {
+	this.intersection = new SimpleStringProperty(entrepot.getIntersection().getId() + " ("
+		+ entrepot.getIntersection().getX() + ", " + entrepot.getIntersection().getY() + ")");
+	this.heureDepart = new SimpleStringProperty(entrepot.getHeureDepart().toString());
+    }
 }
