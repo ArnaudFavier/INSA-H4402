ebbeaebdcc7436b810f6866a6bddb159a914e5ba
Arnaud <arnaudfavier@users.noreply.github.com>
2016-12-02 23:55:41 +0100

    Fully fonctional Modifier Livraison (need improvements)

--- Agile/src/main/java/agile/vue/DialogModifierLivraison.java
+++ Agile/src/main/java/agile/vue/DialogModifierLivraison.java
@@ -1,5 +1,7 @@
 package agile.vue;
 
+import java.time.LocalTime;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXDialog;
@@ -20,18 +22,37 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
+/**
+ * Boite de dialogue permettant de modifier une livraison
+ */
 public final class DialogModifierLivraison {
     private DialogModifierLivraison() {
     }
 
+    /**
+     * Méthode à appeler pour afficher la boite de dialogue de modfication d'une
+     * livraison
+     * 
+     * @param controlleur
+     *            Le controlleur permettant d'accéder à la liste de livraisons,
+     *            et de mettre à jour l'affichage
+     * @param stackPane
+     *            Où la boite de dialogue doit prendre effet
+     * @param livraison
+     *            La livraison à modifier
+     */
     public static void show(ContentController controlleur, StackPane stackPane, Livraison livraison) {
+	// Liste des composants à afficher
 	Label labelPlageDebut = new Label("Plade horaire début :");
 	Label labelPlageFin = new Label("Plade horaire fin :");
 	JFXDatePicker datePickerPlageDebut = new JFXDatePicker();
 	JFXDatePicker datePickerPlageFin = new JFXDatePicker();
 	JFXButton boutonValider = new JFXButton("Valider");
 	JFXButton boutonAnnuler = new JFXButton("Annuler");
+	Text title = new Text("Modifier une livraison");
+	title.setFont(Font.font(null, FontWeight.BOLD, 16));
 
+	// Option des DatePickers
 	datePickerPlageDebut.setPromptText("Début de la plage");
 	datePickerPlageFin.setPromptText("Fin de la plage");
 	datePickerPlageDebut.setDefaultColor(Color.web("#3f51b5"));
@@ -39,6 +60,17 @@ public final class DialogModifierLivraison {
 	datePickerPlageDebut.setShowTime(true);
 	datePickerPlageFin.setShowTime(true);
 
+	// Affectation des heures déjà existantes (s'il y en a)
+	if (livraison.getDebutPlage() != null) {
+	    datePickerPlageDebut
+		    .setTime(LocalTime.of(livraison.getDebutPlage().getHeure(), livraison.getDebutPlage().getMinute()));
+	}
+	if (livraison.getFinPlage() != null) {
+	    datePickerPlageFin
+		    .setTime(LocalTime.of(livraison.getFinPlage().getHeure(), livraison.getFinPlage().getMinute()));
+	}
+
+	// Placement sur une grille
 	GridPane grid = new GridPane();
 	grid.add(labelPlageDebut, 1, 1);
 	grid.add(labelPlageFin, 2, 1);
@@ -63,14 +95,13 @@ public final class DialogModifierLivraison {
 
 	VBox vbox = new VBox();
 	vbox.setPadding(new Insets(5, 10, 0, 10));
-	Text title = new Text("Modifier une livraison");
-	title.setFont(Font.font(null, FontWeight.BOLD, 16));
 	vbox.getChildren().add(title);
 	vbox.getChildren().add(grid);
 	vbox.getChildren().add(hbox);
 
 	JFXDialog dialog = new JFXDialog(stackPane, vbox, JFXDialog.DialogTransition.CENTER);
 
+	// Binding bouton annuler : quitte la fenêtre
 	boutonAnnuler.setOnAction(new EventHandler<ActionEvent>() {
 	    @Override
 	    public void handle(ActionEvent event) {
@@ -78,6 +109,7 @@ public final class DialogModifierLivraison {
 	    }
 	});
 
+	// Binding bouton valider : vérifie les heures et les enregistre
 	boutonValider.setOnAction(new EventHandler<ActionEvent>() {
 	    @Override
 	    public void handle(ActionEvent event) {
@@ -89,9 +121,11 @@ public final class DialogModifierLivraison {
 
 		// Le temps de fin avant le temps de début
 		if (tempsPlageDebut.compareTo(tempsPlageFin) > 0) {
+		    // TODO: afficher un message d'erreur
 		    return;
 		}
 
+		// Enregistrement des modifications
 		livraison.setDebutPlage(tempsPlageDebut);
 		livraison.setFinPlage(tempsPlageFin);
 		controlleur.miseAJourLivraison(ContentController.controlleur.getTournee().getLivraisonsTSP());
