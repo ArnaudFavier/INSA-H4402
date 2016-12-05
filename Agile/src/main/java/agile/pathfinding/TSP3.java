1801752a16e377af2ae7b79341c8f7ffdb92a348
unknown <amoll@IF501-204-02.insa-lyon.fr>
2016-12-05 11:28:12 +0100

    Add TSP3 final

--- Agile/src/main/java/agile/pathfinding/TSP3.java
+++ Agile/src/main/java/agile/pathfinding/TSP3.java
@@ -1,58 +1,49 @@
 package agile.pathfinding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.PriorityQueue;
+import java.util.List;
 
 public class TSP3 extends TemplateTSP {
 
     @Override
     protected Iterator<Integer> iterator(Integer sommetCrt, ArrayList<Integer> nonVus, float[][] cout, int[] duree) {
 
-	    return new IteratorCroissant(nonVus, sommetCrt, cout);
+	    return new IteratorCroissant(nonVus, sommetCrt, cout, duree);
     }
 
     @Override
     protected float bound(Integer sommetCourant, ArrayList<Integer> nonVus, float[][] cout, int[] duree) {
 
-        Float res = 0f;
+        float res = 0f;
+        float min;
 
-        Float min = Float.MAX_VALUE;
-
-        for ( Integer A : nonVus )
-        {
-            if(cout[sommetCourant][A] < min)
-                min = cout[sommetCourant][A];
-        }
+        min = cout[sommetCourant][nonVus.get(0)] + duree[nonVus.get(0)];
+        for(Integer A : nonVus)
+            if(cout[sommetCourant][A] + duree[A] < min)
+                min = cout[sommetCourant][A] + duree[A];
 
         res += min;
 
-        min = Float.MAX_VALUE;
-
-        for ( Integer A : nonVus )
-        {
-            if(cout[A][sommetCourant] < min)
-                min = cout[sommetCourant][A];
-        }
+        min = cout[nonVus.get(0)][0];
+        for(Integer A : nonVus)
+            if(cout[A][0] < min)
+                min = cout[A][0];
 
         res += min;
 
-        min = Float.MAX_VALUE;
+        List<Float> aretesCout = new ArrayList<Float>(nonVus.size()*nonVus.size());
 
-        for ( Integer A : nonVus )
-        {
-            for ( Integer B : nonVus )
-            {
-                if ( A != B )
-                    if(cout[A][B] < min)
-                        min = cout[A][B];
-            }
-        }
+        for(Integer A : nonVus)
+            for(Integer B : nonVus)
+                if(A!=B) {
+                    aretesCout.add(cout[A][B] + duree[B]);
+                }
 
-        res += min * (nonVus.size() - 1);
+        aretesCout.sort(Float::compare);
 
-        //System.out.println(res);
+        for(int i = 0; i < nonVus.size() - 1; i++)
+            res += aretesCout.get(i);
 
         return res;
     }
