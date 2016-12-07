13cd8e14c6106facaabf0397694ec3dab6141fbb
Setny <anthony.delay@outlook.fr>
2016-12-07 09:15:23 +0100

    Pardon Arnaud verision 2.0 : les tests passent

--- Agile/src/test/java/agile/controlleur/CommandeModifierLivraisonTest.java
+++ Agile/src/test/java/agile/controlleur/CommandeModifierLivraisonTest.java
@@ -1,29 +1,55 @@
 package agile.controlleur;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.net.URL;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import agile.modele.DemandeLivraisons;
+import agile.modele.Livraison;
+import agile.modele.Plan;
+import agile.modele.Tournee;
+import agile.xml.DeserialiseurDemandeLivraisonsXML;
+import agile.xml.DeserialiseurPlanXML;
+
 public class CommandeModifierLivraisonTest {
+
+    Tournee tournee;
+    DemandeLivraisons demandeLivraisons;
+    Livraison livraison;
+    Controlleur controlleur;
 
     @Before
     public void setUp() throws Exception {
+
+	URL urlPlan = getClass().getResource("plan5x5.xml");
+	File xmlPlan = new File(urlPlan.toURI());
+	Plan plan = DeserialiseurPlanXML.charger(xmlPlan);
+
+	URL urlDemandeLivraison = getClass().getResource("livraisons5x5-4.xml");
+	File xmlDemandeLivraison = new File(urlDemandeLivraison.toURI());
+	demandeLivraisons = DeserialiseurDemandeLivraisonsXML.charger(xmlDemandeLivraison, plan);
+
+	tournee = new Tournee(demandeLivraisons);
+
+	tournee.calculerTSP();
+	livraison = tournee.getLivraisonsTSP().get(0);
+	controlleur = new Controlleur();
     }
 
     @Test
     public void testCommandeModifierLivraison() {
-	fail("Not yet implemented");
     }
 
     @Test
     public void testDoCde() {
-	fail("Not yet implemented");
+
     }
 
     @Test
     public void testUndoCde() {
-	fail("Not yet implemented");
+
     }
 
 }
