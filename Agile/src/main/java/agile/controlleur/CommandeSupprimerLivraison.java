f28136c4c2c3550346f7e61e718444ab3a1df9af
Setny <anthony.delay@outlook.fr>
2016-12-07 09:08:13 +0100

    Fix heure retour entrepot maj + start test commandeModifier

--- Agile/src/main/java/agile/controlleur/CommandeSupprimerLivraison.java
+++ Agile/src/main/java/agile/controlleur/CommandeSupprimerLivraison.java
@@ -1,6 +1,7 @@
 package agile.controlleur;
 
 import agile.modele.Livraison;
+import agile.modele.Temps;
 import agile.modele.Tournee;
 
 public class CommandeSupprimerLivraison implements Commande {
@@ -8,6 +9,8 @@ public class CommandeSupprimerLivraison implements Commande {
     private Tournee tournee;
     private Livraison livraison;
     private Tournee prevTournee;
+    private int prevHeureRetourEntrepot, heureRetourEntrepot;
+    private boolean alreadyChangeHour;
 
     /**
      * Cree la commande qui permet de supprimer une livraison de la tournee
@@ -18,18 +21,34 @@ public class CommandeSupprimerLivraison implements Commande {
     public CommandeSupprimerLivraison(Tournee tournee, Livraison livraison) {
 	this.tournee = tournee;
 	this.livraison = livraison;
+	alreadyChangeHour = false;
 	prevTournee = tournee.clone();
+	prevHeureRetourEntrepot = tournee.getDemandeInitiale().getEntrepot().getHeureRetour().getTotalSecondes();
     }
 
     @Override
     public void doCde(Controlleur controlleur) {
-	tournee.supprimerLivraison(livraison);
+	if (!alreadyChangeHour) {
+	    tournee.supprimerLivraison(livraison);
+	    alreadyChangeHour = true;
+	    controlleur.setTournee(tournee);
+	    heureRetourEntrepot = controlleur.getTournee().getDemandeInitiale().getEntrepot().getHeureRetour()
+		    .getTotalSecondes();
+
+	} else {
+	    Temps heure = new Temps(heureRetourEntrepot);
+
+	    System.err.println(heure.getTimeString());
+	    tournee.getDemandeInitiale().getEntrepot().setHeureRetour(heure);
+	}
 	controlleur.setTournee(tournee);
 
     }
 
     @Override
     public void undoCde(Controlleur controlleur) {
+	Temps heure = new Temps(prevHeureRetourEntrepot);
+	prevTournee.getDemandeInitiale().getEntrepot().setHeureRetour(heure);
 	controlleur.setTournee(prevTournee);
     }
 
---
+++ Agile/src/test/java/agile/controlleur/CommandeModifierLivraisonTest.java
@@ -0,0 +1,29 @@
+package agile.controlleur;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandeModifierLivraisonTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testCommandeModifierLivraison() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDoCde() {
+	fail("Not yet implemented");
+    }
+
+    @Test
+    public void testUndoCde() {
+	fail("Not yet implemented");
+    }
+
+}
