d84f4bedd7c94b135fb6ed9a91a70ad3c917eea5
Setny <anthony.delay@outlook.fr>
2017-01-17 14:40:13 +0100

    Add custom date and time picker for add_event

--- IHM/Agenda/app/src/main/java/hexanome/agenda/activities/AddEventActivity.java
+++ IHM/Agenda/app/src/main/java/hexanome/agenda/activities/AddEventActivity.java
@@ -1,9 +1,11 @@
 package hexanome.agenda.activities;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.turkialkhateeb.materialcolorpicker.ColorChooserDialog;
 import com.turkialkhateeb.materialcolorpicker.ColorListener;
@@ -15,7 +17,8 @@ public class AddEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event);
-        final Button buttonColor = (android.widget.Button)findViewById(R.id.button_color);
+
+        final Button buttonColor = (Button)findViewById(R.id.button_color);
         buttonColor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,6 +31,54 @@ public class AddEventActivity extends AppCompatActivity {
                 });
                 //customize the dialog however you want
                 dialog.show();
+            }
+        });
+
+        final TextView textViewDateStart = (TextView)findViewById(R.id.txtview_date_start);
+        textViewDateStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(AddEventActivity.this);
+
+                dialog.setContentView(R.layout.date_picker_custom);
+                dialog.show();
+
+            }
+        });
+
+        final TextView textViewDateEnd = (TextView)findViewById(R.id.txtview_date_end);
+        textViewDateEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(AddEventActivity.this);
+
+                dialog.setContentView(R.layout.date_picker_custom);
+                dialog.show();
+
+            }
+        });
+
+        final TextView textViewTimeStart = (TextView)findViewById(R.id.txtview_time_start);
+        textViewTimeStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(AddEventActivity.this);
+
+                dialog.setContentView(R.layout.time_picker_custom);
+                dialog.show();
+
+            }
+        });
+
+        final TextView textViewTimeEnd = (TextView)findViewById(R.id.txtview_time_end);
+        textViewTimeEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(AddEventActivity.this);
+
+                dialog.setContentView(R.layout.time_picker_custom);
+                dialog.show();
+
             }
         });
     }
