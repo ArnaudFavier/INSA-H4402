07af1de3d6b70c66fc2babae5bbe2c7b4b9d3f76
Arnaud <arnaudfavier@users.noreply.github.com>
2017-01-17 19:48:38 +0100

    Prepare list events for a day

--- IHM/Agenda/app/src/main/java/hexanome/agenda/activities/DayActivity.java
+++ IHM/Agenda/app/src/main/java/hexanome/agenda/activities/DayActivity.java
@@ -5,23 +5,65 @@ import android.support.v7.app.AppCompatActivity;
 
 import org.joda.time.DateTime;
 
-import hexanome.agenda.R;
+import java.util.ArrayList;
+import java.util.List;
 
+import hexanome.agenda.R;
+import hexanome.agenda.model.Event;
+import hexanome.agenda.model.ListEvent;
+
+/**
+ * Activity showing events of a particular day
+ */
 public class DayActivity extends AppCompatActivity {
 
+    /**
+     * The day to show events
+     */
     private DateTime day;
+
+    /**
+     * Constructor with the day to show events
+     * @param day The day to show events
+     */
+    public DayActivity(DateTime day) {
+        initialize(day);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day);
 
-        this.initialize();
+        initialize(null);
 
         getSupportActionBar().setTitle(day.toString("EEEE d MMMM"));
+
+        List<Event> events = getListEventToday();
+        // ToDo : Put into the view
     }
 
-    private void initialize() {
-        day = new DateTime();
+    /**
+     * Save the day to show events
+     * @param day The day to show events
+     */
+    private void initialize(DateTime day) {
+        if (day == null)
+            day = new DateTime();
+        this.day = day;
+    }
+
+    /**
+     * Get the list of events during the day
+     * @return A list of events during the day
+     */
+    private List<Event> getListEventToday() {
+        List<Event> events = new ArrayList<>();
+
+        for (Event e : ListEvent.events)
+            if (e.startTime.isEqual(this.day))
+                events.add(e);
+
+        return events;
     }
 }
