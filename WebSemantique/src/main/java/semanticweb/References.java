d410c51640d304a6e8158e6a924b02287d961b30
Arnaud <arnaudfavier@users.noreply.github.com>
2017-01-31 17:41:32 +0100

    Add WS/References.java without API keys

---
+++ WebSemantique/src/main/java/semanticweb/References.java
@@ -0,0 +1,29 @@
+package semanticweb;
+
+/**
+ * Contains some useful global values for this app
+ */
+public final class References {
+	private References() {
+	}
+
+	/**
+	 * The google api key
+	 * @see <a href="https://console.developers.google.com/apis?pro">Google API Console</a>
+	 */
+	public static final String GOOGLE_API_KEY = "XXXXXXXXXXXXXXXXXXXX";
+
+	/**
+	 * The google search engine ID
+	 * @see <a href="https://www.youtube.com/watch?v=vP_inGfKG5E">How to create a Google Search Engine ID</a>
+	 * @see <a href="https://console.developers.google.com/apis?pro">Google API Console</a>
+	 */
+	public static final String GOOGLE_SEARCH_ENGINE_ID = "XXXXXXXXXXXXXXXXXXXX";
+
+	/**
+	 * The IBM Bluemix API key for Watson Alchemy
+	 * @see <a href="https://console.ng.bluemix.net/">Bluemix API Console</a>
+	 * @see <a href="https://developer.ibm.com/students/">Get a promotional Bluemix code for student</a>
+	 */
+	public static final String ALCHEMY_API_KEY = "XXXXXXXXXXXXXXXXXXXX";
+}
